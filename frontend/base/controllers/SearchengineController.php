371d78409a6d41f59815c407fbb62471d38d2b54
John Shearer <john@ultranaut.com>
2015-11-18 17:49:10 -0500

    Disallow indexing the staging site

--- frontend/base/controllers/SearchengineController.php
+++ frontend/base/controllers/SearchengineController.php
@@ -16,35 +16,31 @@ class SearchengineController extends ClientFrontendController
 		// Nothing to see here
 		return $this->_errorRedirect();
 	}
-	
+
 	/**
 	 * Robots.txt
 	 */
 	public function robots()
 	{
 		$https = isset($_SERVER['HTTPS']);
-		
+
 		// Bots don't get secure pages
 		if ($https) {
 			$disallow = array('/');
-			
+
 		// Get list of disallow urls
 		} else {
-		
+
 			// Defaults
 			$disallow = array(
 				'/*...*',
 				'/*?format=print*',
 				'/alphabeticallist',
 				'/oversight/',
-//				'/assets/',
 				'/emailfriend/',
-//				'/images/',
 				'/quick_view/',
 				'/cookbooks',
-//				'/recipe/',
-//				'/article/',
-                '/save_to_recipe_box/',
+				'/save_to_recipe_box/',
 				'/account/',
 				'/profile/',
 				'/forums/',
@@ -69,43 +65,33 @@ class SearchengineController extends ClientFrontendController
 				'/edit/',
 				'/edit_normalize/',
 				'/giveaway2/*test*',
-#				'/recipes/recipes/',
-#				'/recipes/articles/',
-#				'/articles/recipes/',
-#				'/articles/articles/'
 			);
-			
+
 			// Add extras
 			if ($extraDisallow = BluApplication::getSetting('robotsDisallow')) {
 				$extraDisallow = ($extraDisallow);
 				$disallow = array_merge($disallow, $extraDisallow);
 			}
 		}
-		
+
 		// Output
 		$this->_doc->setMimeType('text/plain');
 		$this->_doc->setFormat('raw');
-        echo "User-agent: Mediapartners-Google\r\n";
-        echo "Disallow:\r\n";
-		echo 'User-agent: *'."\r\n";
-         //echo 'Sitemap: http://www.recipe4living.com/sitemap.xml.gz'."\n";
-        //echo 'Sitemap: http://www.recipe4living.com/sitemap.xml'."\n";
-        
-        // Set by Google
-        //echo "http://www.recipe4living.com/sitemap.xml.gz # Added by Google Sitemap Generator" . "\r\n";
-        //
-        
-#        echo "http://www.recipe4living.com/sitemap.xml # Added by Google Sitemap Generator" . "\r\n";
-        
-        //Set by Leon
-        //echo 'Sitemap: http://www.recipe4living.com/sitemap.xml.gz'."\r\n";
-        echo 'Sitemap: http://www.recipe4living.com/xmlsitemap.xml'."\r\n";        
-        
-        
-		foreach ($disallow as $url) {
-			echo 'Disallow: '.$url."\r\n";
+
+    // Please don't index our staging server, ok, thanks.
+		if (defined('STAGING') && STAGING === true) {
+			echo 'User-agent: *'."\r\n";
+			echo 'Disallow: /'."\r\n";
+		}
+		else {
+			echo "User-agent: Mediapartners-Google\r\n";
+			echo "Disallow:\r\n";
+			echo 'User-agent: *'."\r\n";
+			echo 'Sitemap: http://www.recipe4living.com/xmlsitemap.xml'."\r\n";
+
+			foreach ($disallow as $url) {
+				echo 'Disallow: '.$url."\r\n";
+			}
 		}
 	}
 }
-
-?>
