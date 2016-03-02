e15bc5e738b4a43778d55f26a6d09c787ecd3d76
John Shearer <john@ultranaut.com>
2016-03-02 09:53:58 -0500

    Point subctr, unsub and unsubscribe to stage

--- frontend/recipe4living/templates/static/unsub.php
+++ frontend/recipe4living/templates/static/unsub.php
@@ -7,7 +7,7 @@
 						    self.scroll(0,0);
 						}
 					</script>
-					<iframe onload="SubCtrScrollToTop();" src="http://r4l.popularliving.com/subctr/index.php?email=<?php $_GET['e']; ?>&source=<?php $_GET['s']; ?>&subsource=<?php $_GET['ss']; ?>" frameborder="0" width="100%" height="1500"></iframe>
+					<iframe onload="SubCtrScrollToTop();" src="http://stage.r4l.popularliving.com/subctr/index.php?email=<?php $_GET['e']; ?>&source=<?php $_GET['s']; ?>&subsource=<?php $_GET['ss']; ?>" frameborder="0" width="100%" height="1500"></iframe>
 				</td>
 				<td width="300px" valign="top"><div id="panel-right" class="column">
 				<?php include(BLUPATH_TEMPLATES.'/site/newsletter.php') ?>
