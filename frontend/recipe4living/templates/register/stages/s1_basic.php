035d04ffca53cf47c903dc4a454736ce396e41ec
John Shearer <john@ultranaut.com>
2016-06-01 14:45:47 -0400

    Clean up registration template files
    white space and remove dead code

--- frontend/recipe4living/templates/register/stages/s1_basic.php
+++ frontend/recipe4living/templates/register/stages/s1_basic.php
@@ -1,97 +1,78 @@
-				<!--INFOLINKS_OFF-->
-				<form action="<?= SITEURL ?>/register" method="post" class="fullsubmit"><div>
+<!--INFOLINKS_OFF-->
+<form action="<?= SITEURL ?>/register" method="post" class="fullsubmit"><div>
 
-					<dl>
-						<dt><label for="form_username">Username <span class="red-ast">*</span></label></dt>
-						<dd>
-							<input name="form_username" class="textinput minLength validate-alphanum" validatorProps="{minLength: 6}" type="text" id="form_username" size="30" value="<?= $username ?>" />
-							<small class="fieldhint">This name is unique to you and can't be changed later, so please pick something you will remember.</small>
-							<div class="clear"></div>
-						</dd>
-						
-						<dt><label for="form_first_name">First Name <span class="red-ast">*</span></label></dt>
-						<dd>
-							<input name="form_first_name" class="textinput required validate-alphanum" type="text" id="form_first_name" size="30" value="<?= $firstname ?>" />
-						</dd>
-						
-						<dt><label for="form_last_name">Last Name <span class="red-ast">*</span></label></dt>
-						<dd>
-							<input name="form_last_name" class="textinput required validate-alphanum" type="text" id="form_last_name" size="30" value="<?= $lastname ?>" />
-						</dd>
-						
-						<dt><label for="form_email">Email Address <span class="red-ast">*</span></label></dt>
-						<dd>
-							<input name="form_email" class="textinput required validate-email" type="text" id="form_email" value="<?= $email ?>" />
-						</dd>
-	
-						<dt><label for="form_password">Password <span class="red-ast">*</span></label></dt>
-						<dd>
-							<input name="form_password" class="textinput minLength maxLength" validatorProps="{minLength: 6, maxLength: 30}" type="password" id="password" value="" size="30" maxlength="30" />
-						</dd>
-	
-						<dt><label for="form_password_confirm">Confirm Password <span class="red-ast">*</span></label></dt>
-						<dd>
-							<input name="form_password_confirm" class="textinput validate-passwordconfirm" type="password" id="form_password_confirm" />
-						</dd>
-	<?php /* ?>
-						<dt><label for="form_location">Location <span class="red-ast">*</span></label></dt>
-						<dd>
-							<input name="form_location" class="textinput" type="text" id="form_location" size="30" maxlength="100" value="<?= $location ?>" />
-							<small class="fieldhint">Please enter your city / state / country in this field. Please make sure to select your location from the list before moving on to the next field.</small>
-							<?php Template::startScript(); ?>
-	
-								// Location input
-								var locationInput = $('form_location');
-	
-								// Location autocompleter
-								new Autocompleter.Request.JSON(locationInput, SITEURL + '/locations/search/', {postVar: 'criteria'});
-	
-								// Custom validator
-								locationInput.addClass('validate-custom');
-								locationInput.set('validatorProps', JSON.encode({custom_error: 'Please enter a valid location from the autocompleter.', custom_url: SITEURL + '/locations/'}));
-	
-							<?php Template::endScript(); ?>
-						</dd>
-		<?php */ ?>				
-						<dt><label for="form_referral">Where did you hear about Recipe4Living&reg;?</label></dt>
-						<dd>
-							<input name="form_referral" class="textinput" type="text" id="form_referral" size="30" maxlength="100" value="<?= $referral ?>" />
-							<small class="fieldhint">If you were referred by a friend, please type in their email or name in the box above.</small>
-						</dd>
-						
-					</dl>
-					
-					<div class="clear"></div>
-		
-					<div class="captcha">
-						<div class="img">
-							<img src="<?= SITEURL; ?>/captcha?format=asset&uniq=<?= uniqid(); ?>" class="captcha-img" />
-							<div class="captcha-reload"><small><a href="#">Get a new image</a></small></div>
-						</div>
-						<div class="body">
-							<label>Please enter the code to the left:</label>
-							<input name="form_captcha" class="textinput validate-captcha" type="text" id="form_captcha" size="30" maxlength="100" />
-							<small><small>This helps us prevent automatic scripts from spamming you!</small></small>
-						</div>
-	
-						<div class="clear"></div>
-					</div>
+	<dl>
+		<dt><label for="form_username">Username <span class="red-ast">*</span></label></dt>
+		<dd>
+			<input name="form_username" class="textinput minLength validate-alphanum" validatorProps="{minLength: 6}" type="text" id="form_username" size="30" value="<?= $username ?>" />
+			<small class="fieldhint">This name is unique to you and can't be changed later, so please pick something you will remember.</small>
+			<div class="clear"></div>
+		</dd>
 
-					<p>
-						<label class="check"><input name="form_newsletter" class="check" type="checkbox" id="form_newsletter" value="y"<?php if ($newsletter) { echo ' checked="checked"'; } ?> />
-						<?= Template::text('registration_newsletter'); ?></label>
-					</p>
-					<div class="clear"></div>
+		<dt><label for="form_first_name">First Name <span class="red-ast">*</span></label></dt>
+		<dd>
+			<input name="form_first_name" class="textinput required validate-alphanum" type="text" id="form_first_name" size="30" value="<?= $firstname ?>" />
+		</dd>
 
-					<p>
-						<label class="check"><input name="form_terms" class="check validate-terms-required" type="checkbox" id="form_terms" value="y" />
-						Yes, I agree to the Recipe4Living&reg; <a href="<?= SITEURL ?>/terms" class="info-popup">terms and conditions</a></label>
-					</p>
-					<div class="clear" style="height:10px;"></div>
-	
-					<button type="submit" class="button-md fl"><span>Continue</span></button>
-					<div class="clear"></div>
-	
-					<input type="hidden" name="task" value="s1_basic_save" />
-				</div></form>
-				<!--INFOLINKS_ON-->
+		<dt><label for="form_last_name">Last Name <span class="red-ast">*</span></label></dt>
+		<dd>
+			<input name="form_last_name" class="textinput required validate-alphanum" type="text" id="form_last_name" size="30" value="<?= $lastname ?>" />
+		</dd>
+
+		<dt><label for="form_email">Email Address <span class="red-ast">*</span></label></dt>
+		<dd>
+			<input name="form_email" class="textinput required validate-email" type="text" id="form_email" value="<?= $email ?>" />
+		</dd>
+
+		<dt><label for="form_password">Password <span class="red-ast">*</span></label></dt>
+		<dd>
+			<input name="form_password" class="textinput minLength maxLength" validatorProps="{minLength: 6, maxLength: 30}" type="password" id="password" value="" size="30" maxlength="30" />
+		</dd>
+
+		<dt><label for="form_password_confirm">Confirm Password <span class="red-ast">*</span></label></dt>
+		<dd>
+			<input name="form_password_confirm" class="textinput validate-passwordconfirm" type="password" id="form_password_confirm" />
+		</dd>
+
+		<dt><label for="form_referral">Where did you hear about Recipe4Living&reg;?</label></dt>
+		<dd>
+			<input name="form_referral" class="textinput" type="text" id="form_referral" size="30" maxlength="100" value="<?= $referral ?>" />
+			<small class="fieldhint">If you were referred by a friend, please type in their email or name in the box above.</small>
+		</dd>
+
+	</dl>
+
+	<div class="clear"></div>
+
+	<div class="captcha">
+		<div class="img">
+			<img src="<?= SITEURL; ?>/captcha?format=asset&uniq=<?= uniqid(); ?>" class="captcha-img" />
+			<div class="captcha-reload"><small><a href="#">Get a new image</a></small></div>
+		</div>
+		<div class="body">
+			<label>Please enter the code to the left:</label>
+			<input name="form_captcha" class="textinput validate-captcha" type="text" id="form_captcha" size="30" maxlength="100" />
+			<small><small>This helps us prevent automatic scripts from spamming you!</small></small>
+		</div>
+
+		<div class="clear"></div>
+	</div>
+
+	<p>
+		<label class="check"><input name="form_newsletter" class="check" type="checkbox" id="form_newsletter" value="y"<?php if ($newsletter) { echo ' checked="checked"'; } ?> />
+		<?= Template::text('registration_newsletter'); ?></label>
+	</p>
+	<div class="clear"></div>
+
+	<p>
+		<label class="check"><input name="form_terms" class="check validate-terms-required" type="checkbox" id="form_terms" value="y" />
+		Yes, I agree to the Recipe4Living&reg; <a href="<?= SITEURL ?>/terms" class="info-popup">terms and conditions</a></label>
+	</p>
+	<div class="clear" style="height:10px;"></div>
+
+	<button type="submit" class="button-md fl"><span>Continue</span></button>
+	<div class="clear"></div>
+
+	<input type="hidden" name="task" value="s1_basic_save" />
+</div></form>
+<!--INFOLINKS_ON-->
--- frontend/recipe4living/templates/register/stages/s2_optional.php
+++ frontend/recipe4living/templates/register/stages/s2_optional.php
@@ -1,92 +1,91 @@
 <!--INFOLINKS_OFF-->
-	<div class="wrapper">
-		<div class="content">
-			<form action="<?= SITEURL ?>/register" method="post" enctype="multipart/form-data" class="fullsubmit"><div>
+<div class="wrapper">
+  <div class="content">
+    <form action="<?= SITEURL ?>/register" method="post" enctype="multipart/form-data" class="fullsubmit"><div>
 
-				<p class="text-content" style="margin-bottom: 10px;">
-				The following information is not required but will help you have a richer experience at <?= BluApplication::getSetting('storeName'); ?>. If you'd like to fill it out later, just click the <strong>'complete sign up'</strong> at the bottom of this page.
-				</p>
+      <p class="text-content" style="margin-bottom: 10px;">
+      The following information is not required but will help you have a richer experience at <?= BluApplication::getSetting('storeName'); ?>. If you'd like to fill it out later, just click the <strong>'complete sign up'</strong> at the bottom of this page.
+      </p>
 
-				<div class="divider"></div>
+      <div class="divider"></div>
 
-				<h3>A bit about you</h3>
-				<dl>
-					
-					<dt><label for="form_location">Location</label></dt>
-					<dd>
-						<input name="form_location" class="textinput"  type="text" id="form_location" size="30" value="<?= $location ?>" />
-						<small class="fieldhint">(Example: Chicago, IL)</small>
-					<dt><label for="form_favourite_foods">Favorite Foods</label></dt>
-					<dd>
-						<textarea name="form_favourite_foods" class="textinput simpletext" id="form_favourite_foods" title="Examples: pizza, pasta, sushi" rows="2" cols="30"><?= $favouriteFoods ?></textarea>
-					</dd>
-					<dt><label for="form_about">A little bit about you</label></dt>
-					<dd>
-						<textarea name="form_about" class="textinput" id="form_about" rows="5" cols="30"><? Template::out($about); ?></textarea>
-					</dd>
-					<dt><label for="form_dob">Your birthday</label></dt>
-					<dd>
-						<select name="form_dob_month" id="form_dob_year" class="dates">
-							<option value="">Month</option>
-							<?php for ($i = 1; $i <= 12; $i++) { ?>
-							<option value="<?= $i ?>" <?= ($dobMonth == $i) ? 'selected="selected"' : '' ?>><?= $monthNames[$i] ?></option>
-							<?php } ?>
-						</select>
+      <h3>A bit about you</h3>
+      <dl>
 
-						<select name="form_dob_day" id="form_dob_day" class="dates">
-							<option value="">Day</option>
-							<?php for ($i = 1; $i <= 31; $i++) { ?>
-							<option value="<?= $i ?>" <?= ($dobDay == $i) ? 'selected="selected"' : '' ?>><?= $i ?></option>
-							<?php } ?>
-						</select>
-						
-						<select name="form_dob_year" id="form_dob_year" class="dates">
-							<option value="">Year</option>
-							<?php for ($i = 1920; $i <= date('Y'); $i++) { ?>
-							<option value="<?= $i ?>" <?= ($dobYear == $i) ? 'selected="selected"' : '' ?>><?= $i ?></option>
-							<?php } ?>
-						</select>
-					</dd>
+        <dt><label for="form_location">Location</label></dt>
+        <dd>
+          <input name="form_location" class="textinput"  type="text" id="form_location" size="30" value="<?= $location ?>" />
+          <small class="fieldhint">(Example: Chicago, IL)</small>
+        <dt><label for="form_favourite_foods">Favorite Foods</label></dt>
+        <dd>
+          <textarea name="form_favourite_foods" class="textinput simpletext" id="form_favourite_foods" title="Examples: pizza, pasta, sushi" rows="2" cols="30"><?= $favouriteFoods ?></textarea>
+        </dd>
+        <dt><label for="form_about">A little bit about you</label></dt>
+        <dd>
+          <textarea name="form_about" class="textinput" id="form_about" rows="5" cols="30"><? Template::out($about); ?></textarea>
+        </dd>
+        <dt><label for="form_dob">Your birthday</label></dt>
+        <dd>
+          <select name="form_dob_month" id="form_dob_year" class="dates">
+            <option value="">Month</option>
+            <?php for ($i = 1; $i <= 12; $i++) { ?>
+            <option value="<?= $i ?>" <?= ($dobMonth == $i) ? 'selected="selected"' : '' ?>><?= $monthNames[$i] ?></option>
+            <?php } ?>
+          </select>
 
-					<dt></dt>
-					<dd>
-						<label for="form_private" class="check" style="margin-bottom: 15px;"><input name="form_private" id="form_private" value="1" type="checkbox" <?= ($private) ? 'checked="checked"' : ''; ?>/>Make my profile private</label>
-					</dd>
-				</dl>
-				<div class="clear"></div>
+          <select name="form_dob_day" id="form_dob_day" class="dates">
+            <option value="">Day</option>
+            <?php for ($i = 1; $i <= 31; $i++) { ?>
+            <option value="<?= $i ?>" <?= ($dobDay == $i) ? 'selected="selected"' : '' ?>><?= $i ?></option>
+            <?php } ?>
+          </select>
 
-				<div class="divider"></div>
+          <select name="form_dob_year" id="form_dob_year" class="dates">
+            <option value="">Year</option>
+            <?php for ($i = 1920; $i <= date('Y'); $i++) { ?>
+            <option value="<?= $i ?>" <?= ($dobYear == $i) ? 'selected="selected"' : '' ?>><?= $i ?></option>
+            <?php } ?>
+          </select>
+        </dd>
 
-				<h3>Would you like to add a photo to your profile?</h3>
-				<div class="fieldwrap photo">
-					<ul>
-						<li>
-							<label for="form_imgfile">Upload a photo from your computer</label>
-							<input type="file" name="photoupload" id="photoupload" class="file text-content" size="50" />
-						</li>
-						<li>
-							<label>OR, choose an avatar below:</label>
-							<div class="imageradios">
-								<label><img src="<?= ASSETURL ?>/userimages/60/60/1/avatar1.png" />
-									<input type="radio" name="avatar" value="1" checked="checked" /></label>
-								<label><img src="<?= ASSETURL ?>/userimages/60/60/1/avatar2.png" />
-									<input type="radio" name="avatar" value="2" /></label>
-								<label><img src="<?= ASSETURL ?>/userimages/60/60/1/avatar3.png" />
-									<input type="radio" name="avatar" value="3" /></label>
-							</div>
-							<div class="clear"></div>
-						</li>
-					</ul>
-				</div>
+        <dt></dt>
+        <dd>
+          <label for="form_private" class="check" style="margin-bottom: 15px;"><input name="form_private" id="form_private" value="1" type="checkbox" <?= ($private) ? 'checked="checked"' : ''; ?>/>Make my profile private</label>
+        </dd>
+      </dl>
+      <div class="clear"></div>
 
-				<div class="divider"></div>
+      <div class="divider"></div>
 
-				<div align="center"><button type="submit" class="button-md fl"><span>Complete sign up and go to my account</span></button></div>
+      <h3>Would you like to add a photo to your profile?</h3>
+      <div class="fieldwrap photo">
+        <ul>
+          <li>
+            <label for="form_imgfile">Upload a photo from your computer</label>
+            <input type="file" name="photoupload" id="photoupload" class="file text-content" size="50" />
+          </li>
+          <li>
+            <label>OR, choose an avatar below:</label>
+            <div class="imageradios">
+              <label><img src="<?= ASSETURL ?>/userimages/60/60/1/avatar1.png" />
+                <input type="radio" name="avatar" value="1" checked="checked" /></label>
+              <label><img src="<?= ASSETURL ?>/userimages/60/60/1/avatar2.png" />
+                <input type="radio" name="avatar" value="2" /></label>
+              <label><img src="<?= ASSETURL ?>/userimages/60/60/1/avatar3.png" />
+                <input type="radio" name="avatar" value="3" /></label>
+            </div>
+            <div class="clear"></div>
+          </li>
+        </ul>
+      </div>
 
-				<input type="hidden" name="task" value="s2_optional_save" />
-				<input type="hidden" id="queueid" name="queueid" value="<?= $queueId ?>" />
-			</div></form>
-		</div>
-	</div>
-	<!--INFOLINKS_ON-->
-	
+      <div class="divider"></div>
+
+      <div align="center"><button type="submit" class="button-md fl"><span>Complete sign up and go to my account</span></button></div>
+
+      <input type="hidden" name="task" value="s2_optional_save" />
+      <input type="hidden" id="queueid" name="queueid" value="<?= $queueId ?>" />
+    </div></form>
+  </div>
+</div>
+<!--INFOLINKS_ON-->
