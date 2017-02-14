2da233f185ab49aa9a1867dfa5380b84bb00f7c1
John Shearer <john@ultranaut.com>
2017-02-14 11:42:02 -0500

    Break right rail ads out into separate template files

---
+++ frontend/recipe4living/templates/site/ads/netseer_2358.php
@@ -0,0 +1,10 @@
+<div class="screenonly"><br>
+	<script type="text/javascript">
+		netseer_tag_id = "2358";
+		netseer_ad_width = "300";
+		netseer_ad_height = "100";
+		netseer_task = "ad";
+	</script>
+	<script src="http://contextlinks.netseer.com/dsatserving2/scripts/netseerads.js" type="text/javascript"></script>
+</div>
+
