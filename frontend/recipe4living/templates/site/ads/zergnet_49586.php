cb3043265b3242c9ec7f30faeab1ff5031a433f7
John Shearer <john@ultranaut.com>
2017-02-14 12:17:57 -0500

    Swap out rail Zergnet tag

--- frontend/recipe4living/templates/site/ads/zergnet_49586.php
+++ frontend/recipe4living/templates/site/ads/zergnet_49586.php
@@ -1,16 +1,16 @@
 <!-- zergnet -->
 <div class="ad">
 	<div style="font-family:Arial color:#000000; font-size:18px; font-weight:bold !important;">Around The Web</div>
-	<div id="zergnet-widget-29019"></div>
+  <div id="zergnet-widget-49586"></div>
 </div>
 
-	<script language="javascript" type="text/javascript">
-		(function() {
-			var zergnet = document.createElement('script');
-			zergnet.type = 'text/javascript'; zergnet.async = true;
-			zergnet.src = 'http://www.zergnet.com/zerg.js?id=29019';
-			var znscr = document.getElementsByTagName('script')[0];
-			znscr.parentNode.insertBefore(zergnet, znscr);
-		})();
-	</script>
+<script language="javascript" type="text/javascript">
+  (function() {
+    var zergnet = document.createElement('script');
+    zergnet.type = 'text/javascript'; zergnet.async = true;
+    zergnet.src = (document.location.protocol == "https:" ? "https:" : "http:") + '//www.zergnet.com/zerg.js?id=49586';
+    var znscr = document.getElementsByTagName('script')[0];
+    znscr.parentNode.insertBefore(zergnet, znscr);
+  })();
+</script>
 <!-- /zergnet -->
