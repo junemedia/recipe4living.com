2153a8657d922fba1824b9d857b900002b04bbd0
John Shearer <john@ultranaut.com>
2017-02-15 13:42:51 -0500

    Swap out Netseer rail unit for Media.net

--- frontend/recipe4living/templates/site/ads/WEBSITE_RIGHT_BANNER_1.php
+++ frontend/recipe4living/templates/site/ads/WEBSITE_RIGHT_BANNER_1.php
@@ -1,5 +1,5 @@
 <?php if (!isset($_GET['cid'])) { ?>
 	<?php include BLUPATH_TEMPLATES.'/site/ads/openx_300x250btf.php'; ?>
 	<?php include BLUPATH_TEMPLATES.'/site/ads/zergnet_49586.php'; ?>
-	<?php include BLUPATH_TEMPLATES.'/site/ads/netseer_2358.php'; ?>
+	<?php include BLUPATH_TEMPLATES.'/site/ads/medianet_rail_300x125.php'; ?>
 <?php } ?>
