d8bccc1cef18a510eb6cce6640a5a8b82b64e2bc
John Shearer <john@ultranaut.com>
2017-04-22 15:13:45 -0400

    Comment out bMobilized javascript
    Their server seems to be having issues...

--- frontend/recipe4living/templates/site/header.php
+++ frontend/recipe4living/templates/site/header.php
@@ -4,12 +4,12 @@
 <head>
 <meta http-equiv="x-ua-compatible" content="IE=edge" />
 	<script type="text/javascript">
-    var mobile_domain ="m.recipe4living.com";
-    // Set to false to not redirect on iPad.
-    var ipad = false;
-    // Set to false to not redirect on other tablets (Android , BlackBerry, WebOS tablets).
-    var other_tablets = false;
-    document.write(unescape("%3Cscript src='"+location.protocol+"//s3.amazonaws.com/me.static/js/me.redirect.min.js' type='text/javascript'%3E%3C/script%3E"));
+    /* var mobile_domain ="m.recipe4living.com"; */
+    /* // Set to false to not redirect on iPad. */
+    /* var ipad = false; */
+    /* // Set to false to not redirect on other tablets (Android , BlackBerry, WebOS tablets). */
+    /* var other_tablets = false; */
+    /* document.write(unescape("%3Cscript src='"+location.protocol+"//s3.amazonaws.com/me.static/js/me.redirect.min.js' type='text/javascript'%3E%3C/script%3E")); */
 	</script>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
