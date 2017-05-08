70da3acbf8388c5d932059cdf0b88ad88be07e2c
John Shearer <john@ultranaut.com>
2017-05-08 15:19:38 -0400

    Whitespace cleanup on ad files

--- frontend/recipe4living/templates/site/ads/WEBSITE_TOP_NAV.php
+++ frontend/recipe4living/templates/site/ads/WEBSITE_TOP_NAV.php
@@ -1,33 +1,27 @@
 <div class="screenonly">
 
-	<script type="text/javascript">
-		var LB_ATF_Params = { ybot_slot:"LB_ATF", ybot_size:"", ybot_cpm:"" };
-		try {
-			LB_ATF_Params = yieldbot.getSlotCriteria('LB_ATF');
-		}
-		catch(e) {/*ignore*/}
-	</script>
+  <script type="text/javascript">
+    var LB_ATF_Params = { ybot_slot:"LB_ATF", ybot_size:"", ybot_cpm:"" };
+    try {
+      LB_ATF_Params = yieldbot.getSlotCriteria('LB_ATF');
+    }
+    catch(e) {/*ignore*/}
+  </script>
 
-	<!--/* OpenX Asynchronous JavaScript tag */-->
-	<div id="537229262_728x90ATF" style="width:728px;height:90px;margin:0;padding:0">
-		<noscript>
-			<iframe id="f17dd02004" name="f17dd02004" src="//junemedia-d.openx.net/w/1.0/afr?auid=537229262&cb=INSERT_RANDOM_NUMBER_HERE" frameborder="0" scrolling="no" width="728" height="90">
-				<a href="//junemedia-d.openx.net/w/1.0/rc?cs=f17dd02004&cb=INSERT_RANDOM_NUMBER_HERE" >
-					<img src="//junemedia-d.openx.net/w/1.0/ai?auid=537229262&cs=f17dd02004&cb=INSERT_RANDOM_NUMBER_HERE" border="0" alt="">
-				</a>
-			</iframe>
-		</noscript>
-	</div>
-	<script type="text/javascript">
-		var OX_ads = OX_ads || [];
-		OX_ads.push({
-			slot_id: "537229262_728x90ATF",
-			auid: "537229262",
-			vars: { "ybot_slot":LB_ATF_Params.ybot_slot, "ybot_size": LB_ATF_Params.ybot_size, "ybot_cpm": LB_ATF_Params.ybot_cpm }
-		});
-	</script>
+  <!--/* OpenX Asynchronous JavaScript tag */-->
+  <div id="537229262_728x90ATF" style="width:728px;height:90px;margin:0;padding:0">
+    <noscript><iframe id="f17dd02004" name="f17dd02004" src="//junemedia-d.openx.net/w/1.0/afr?auid=537229262&cb=INSERT_RANDOM_NUMBER_HERE" frameborder="0" scrolling="no" width="728" height="90"><a href="//junemedia-d.openx.net/w/1.0/rc?cs=f17dd02004&cb=INSERT_RANDOM_NUMBER_HERE" ><img src="//junemedia-d.openx.net/w/1.0/ai?auid=537229262&cs=f17dd02004&cb=INSERT_RANDOM_NUMBER_HERE" border="0" alt=""></a></iframe></noscript>
+  </div>
+  <script type="text/javascript">
+    var OX_ads = OX_ads || [];
+    OX_ads.push({
+      slot_id: "537229262_728x90ATF",
+      auid: "537229262",
+      vars: { "ybot_slot":LB_ATF_Params.ybot_slot, "ybot_size": LB_ATF_Params.ybot_size, "ybot_cpm": LB_ATF_Params.ybot_cpm }
+    });
+  </script>
 
-	<script type="text/javascript" src="//junemedia-d.openx.net/w/1.0/jstag"></script>
-	<!-- end openx 728x90ATF -->
+  <script type="text/javascript" src="//junemedia-d.openx.net/w/1.0/jstag"></script>
+  <!-- end openx 728x90ATF -->
 
 </div>
