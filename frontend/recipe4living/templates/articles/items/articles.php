dde28baa631f29641f395eed10a297c498a820a6
John Shearer <john@ultranaut.com>
2017-05-15 16:06:19 -0400

    Cleanup article items template

--- frontend/recipe4living/templates/articles/items/articles.php
+++ frontend/recipe4living/templates/articles/items/articles.php
@@ -1,35 +1,3 @@
-	<?php if (strstr($listingBaseUrl, 'global_cuisines/indian') || strstr($listingBaseUrl, 'global_cuisines/italian') || strstr($listingBaseUrl, 'global_cuisines/mexican') || strstr($listingBaseUrl, 'crockpot') || strstr($listingBaseUrl, 'casseroles')  || strstr($listingBaseUrl, 'budget_cooking') || strstr($listingBaseUrl, 'special_occasion') || strstr($listingBaseUrl, 'quick_easy') || strstr($listingBaseUrl, 'global_cuisines/mexican') || strstr($listingBaseUrl, 'crockpot') || strstr($listingBaseUrl, 'casseroles')  || strstr($listingBaseUrl, 'budget_cooking') || strstr($listingBaseUrl, 'special_occasion') || strstr($listingBaseUrl, 'copycat') ) { ?>
-	<!--<div style="padding-bottom:10px; padding-top:1px;">
-		<?php if (strstr($listingBaseUrl, 'global_cuisines/mexican')) { ?>
-			<a href="http://www.mexicanrecipe4living.com/" target="_blank"><img src="http://pics.recipe4living.com/Mexican_Banner_3_R4L.jpg" border="0"></a>
-		<?php } ?>
-		<?php if (strstr($listingBaseUrl, 'global_cuisines/indian')) { ?>
-			<a href="http://www.indianrecipe4living.com/" target="_blank"><img src="http://pics.recipe4living.com/Indian_Banner_3_R4L.jpg" border="0"></a>
-		<?php } ?>
-		<?php if (strstr($listingBaseUrl, 'global_cuisines/italian')) { ?>
-			<a href="http://www.italianrecipe4living.com/" target="_blank"><img src="http://pics.recipe4living.com/Italian_Banner_3_R4L.jpg" border="0"></a>
-		<?php } ?>
-		<?php if (strstr($listingBaseUrl, 'crockpot')) { ?>
-		<a href="http://www.answerforprizes.com/dispatch2.asp?home=1787-28907L-L22" target="_blank"><img src="http://pics.recipe4living.com/CrockPot-BannerA.jpg" border="0"></a>
-		<?php } ?>
-				<?php if (strstr($listingBaseUrl, 'casseroles')) { ?>
-		<a href="http://www.tellusdaily.com/dispatch2.asp?home=1819-29015U-L1" target="_blank"><img src="http://pics.recipe4living.com/Casserole-BannerA.jpg" border="0"></a>
-		<?php } ?>
-		<?php if (strstr($listingBaseUrl, 'budget_cooking')) { ?>
-		<a href="http://www.dailygiftsforinput.com/dispatch2.asp?home=1805-28941A-L13" target="_blank"><img src="http://pics.recipe4living.com/Budget-BannerA.jpg" border="0"></a>
-		<?php } ?>
-		<?php if (strstr($listingBaseUrl, 'special_occasion')) { ?>
-		<a href="http://www.supersurveysdaily.com/dispatch2.asp?home=1792-28910I-L5" target="_blank"><img src="http://pics.recipe4living.com/Party-BannerA.jpg" border="0"></a>
-		<?php } ?>
-		<?php if (strstr($listingBaseUrl, 'quick_easy')) { ?>
-		<a href="http://www.surveysformore.com/dispatch2.asp?home=1804-28926Q-L6" target="_blank"><img src="http://pics.recipe4living.com/QNE-BannerA.jpg" border="0"></a>
-		<?php } ?>
-		<?php if (strstr($listingBaseUrl, 'copycat')) { ?>
-		<a href="http://www.weeklyopinionstwo.com/dispatch2.asp?home=1884-29377B" target="_blank"><img src="http://pics.recipe4living.com/copycat_NL.jpg" border="0"></a>
-		<?php } ?>
-	</div>-->
-	<?php } ?>
-
 	<div id="list-heading" class="rounded">
 		<div class="content">
 			<h1 style="font-size: 1.25em; color: #FFFFFF; margin-bottom: 0; line-height: 1.25em;<?php echo $iscategory?'float:left;':'';?>">Recipes - <?= $listingTitle; ?> (<?php echo number_format($pagination->get('total')); ?>)</h1>
@@ -60,13 +28,13 @@
 	</div>
 	<?php }?>
 	<div id="recipes_listing">
-	
+
 		<?= Messages::getMessages(); ?>
-		
+
 		<?php if (!empty($items)) { ?>
-		
+
 		<form id="listing-recipe-search" action="<?= SITEURL.$listingBaseUrl; ?>" method="get" class="reloads"><div>
-						
+
 			<div id="list-sort" class="fl">
 				<label for="sort" class="fl">Sort by:</label>
 				<select id="sort-by" name="sort" class="reloads">
@@ -78,7 +46,7 @@
 				</select>
 					<input type="submit" value="Go" />
 			</div>
-			
+
 			<div class="recipe-view fr">
 				<strong class="fl">View:</strong>
 				<ul>
@@ -86,17 +54,17 @@
 					<li id="view-gallery"><a class="reloads<?= $layout == 'grid' ? ' current' : ''; ?>" href="<?= SITEURL.$layoutBaseUrl ?>grid">Grid</a></li>
 				</ul>
 			</div>
-			
+
 			<div class="clear"></div>
-			
+
 			<?php if ($showSearch) { ?><input type="hidden" name="searchterm" value="<?= $searchTerm ?>" /><?php } ?>
 			<?php if ($showSearchExtra) { ?><input type="hidden" name="searchterm_extra" value="<?= $searchTermExtra; ?>" /><?php } ?>
 			<input type="hidden" name="page" value="<?= $page ?>" />
-			
+
 		</div></form>
-		
+
 		<div class="clear"></div>
-		
+
 		<div class="thumb-list">
 			<ul style="<?php echo $iscategory?'width:640px':'';?>">
 			<?php
@@ -108,13 +76,13 @@
 					$recipeBoxRemoveLink = $itemsModel->getTaskLink($item['link'], 'remove_from_recipe_box');
 					$recipeBoxRemoveLink .= isset($listingBaseUrl) ? '?redirect='.base64_encode(SITEURL.$listingBaseUrl) : '';
 					$recipeNoteLink = $itemsModel->getTaskLink($item['link'], 'save_recipe_note');
-					
+
 					if ($item['author']['username'] == '') { $item['author']['username'] = 'Recipe4Living'; }
-					
+
 					// Display
 					switch ($layout) {
 						case 'list':
-			?>			
+			?>
 			<li class="list">
 				<?php if (strstr($item['image']['filename'],'avatar')) { $item['image']['filename']='';} if (isset($item['thumbnail']['filename']) || (!isset($item['thumbnail']['filename']) && $item['image']['filename'] !='')) { ?>
 				<div class="im fl">
@@ -123,60 +91,39 @@
 					</a>
 				</div>
 				<?php } ?>
-				
+
 				<div class="desc" style="<?php echo $iscategory?'width:495px;':'';?>">
 					<h2 class="fl"><a href="<?= SITEURL.$item['link']; ?>"><?= $item['title']; ?></a></h2>
-					
+
 					<div class="rating fl">
 						<?php if (!strstr($item['link'], '/articles/')) { include(BLUPATH_TEMPLATES.'/articles/items/rating.php'); } ?>
 					</div>
-					
+
 					<div class="clear"></div>
-					
+
 					<?php if (isset($item['thumbnail']['filename']) || (!isset($item['thumbnail']['filename']) && $item['image']['filename'] !='')) { ?>
 						<div class="text-content">
 					<?php } else { ?>
 						<div class="text-content" style="width:470px;">
 					<?php } ?>
 						<p><?= Text::trim($item['teaser'], 200); ?></p>
-						<!--<div class="shared-by fl">
-							<?php if ($item['author']['username'] != 'Campbells') { ?>
-								Shared by 
-								<?php if ($item['author']) { ?>
-								<a href="<?= SITEURL; ?>/profile/<?= $item['author']['username']; ?>"><?= $item['author']['username']; ?></a>
-								<?php } else { ?>
-								<?= Template::text('global_anon_user'); ?>
-								<?php } ?>
-							<?php } ?>
-							
-							<?php
-								$format = $this->_doc->getFormat();
-								if (in_array($item['id'],array('38128','38129','38131','42925','48373','48375','48376','48378','48380','48382','48384','48385','48387','48388','48389','48391','48392','48395','48397','48398','48400','48402','48404','48406','48407','48409','48410','48412','48413','48414','48415','48416','48417','48418','48419','48420','48422','48423','48425','48426','48429','48431','48432','48434','48436','48439','48394','38180','48451','48452','48446','48444','48445','48441'))) {
-									if ($format == 'print') {
-										echo "<img src='http://bs.serving-sys.com/BurstingPipe/adServer.bs?cn=tf&c=19&mc=imp&pli=3197103&PluID=0&ord=[timestamp]&rtu=-1' width='0' height='0' border='0' />";
-									}
-								}
-							?>
-							
-							
-						</div>-->
 						<div class="fr">
 							<div class="views fl"><?= $item['views']; ?></div>
-							<?php 
+							<?php
 								switch ($item['type']) {
 									case 'recipe':
 										// Bit of a bodge, should really duplicate whole file into another template: /recipes/items/recipes.php
 										if ($item['inRecipeBox']) {
 							?>
 							<a class="recipe-box-remove fl" title="Remove from recipe box" href="<?= SITEURL.$recipeBoxRemoveLink; ?>"></a>
-							<?php 
-										} else { 
+							<?php
+										} else {
 							?>
 							<a class="recipe-box-add fl" title="Add to recipe box" href="<?= SITEURL.$recipeBoxLink; ?>"></a>
 							<?php
 										}
 										break;
-										
+
 									default:
 										break;
 								}
@@ -184,16 +131,16 @@
 						</div>
 
 					</div>
-					
+
 					<div class="clear"></div>
-					
+
 					<?php if ($item['type'] == 'recipe' && !empty($item['recipe_note'])) {  ?>
 					<div class="message message-info recipe-note"><?= htmlspecialchars($item['recipe_note']) ?></div>
 					<?php } ?>
 				</div>
-				
+
 				<div class="clear"></div>
-				
+
 			</li>
 			<?php
 						break;
@@ -214,33 +161,26 @@
 						<img alt="<?= (!empty($item['default_alt']))?$item['default_alt']:'';?>" src="<?= ASSETURL; ?>/itemimages/<?= $imgsize;?>/<?= $imgsize;?>/<?= $imgtype;?>/<?= $item['image']['filename']; ?>" width="<?= $imgwidth;?>" height="<?= $imgwidth;?>" />
 					</a>
 				</div>
-				
+
 				<div class="desc">
 					<h2><a href="<?= SITEURL.$item['link']; ?>"><?= Text::trim($item['title'], 25); ?></a></h2>
-					
+
 					<div class="text-content">
-						<!--<div class="shared-by">
-							<?php if ($item['author']) { ?>
-							<a href="<?= SITEURL; ?>/profile/<?= $item['author']['username']; ?>"><?= $item['author']['username']; ?></a>
-							<?php } else { ?>
-							<?= Template::text('global_anon_user'); ?>
-							<?php } ?>
-						</div>-->
 						<div class="views fl"><?= $item['views']; ?></div>
-						
+
 						<div class="rating fl">
 							<?php include(BLUPATH_TEMPLATES.'/articles/items/rating.php'); ?>
 						</div>
-						
+
 						<div class="clear"></div>
 					</div>
-					
+
 				</div>
-				
+
 				<div class="clear"></div>
-				
+
 			</li>
-			<?php 
+			<?php
 						break;
 					}
           // place Lockerdome unit after 6th item (2nd row of grid view)
@@ -252,11 +192,11 @@
 			</ul>
 			<div class="clear"></div>
 		</div>
-		
+
 		<?= $pagination->get('buttons', array(
 			'pre' => '<strong class="fl">Pages: </strong>'
 		)); ?>
 
 		<?php } ?>
-		
+
 	</div>
