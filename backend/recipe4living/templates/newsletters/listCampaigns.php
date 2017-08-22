3b7f6a863b6be8541f6c1b385c3c4cf374c6a18e
John Shearer <john@ultranaut.com>
2017-08-22 16:06:02 -0400

    Fix campaigns list page when there are no upcoming campaigns

--- backend/recipe4living/templates/newsletters/listCampaigns.php
+++ backend/recipe4living/templates/newsletters/listCampaigns.php
@@ -1,41 +1,37 @@
-<?php if(!empty($campaigns)) { ?>
+<table id="items_data" class="centered horizontal" style="width: 1200px">
 
-  <table id="items_data" class="centered horizontal" style="width: 1200px">
+  <tr class="metadata">
+    <td colspan="4" style="border: none;">
+      <div style="height: 10px; margin: 23px 0px 5px;">
+        <a href="<?php echo SITEURL . "/newsletters/{$this->_newsletter}/new" ?>" style="font-weight:bold;text-decoration:none;">+ New campaign</a>
+      </div>
+    </td>
+  </tr>
 
-    <tr class="metadata">
-      <td colspan="3" style="border: none;">
-        <div style="height: 10px; margin: 23px 0px 5px;">
-          <a href="<?php echo SITEURL . "/newsletters/{$this->_newsletter}/new" ?>" style="font-weight:bold;text-decoration:none;">+ New campaign</a></div>
-      </td>
-    </tr>
+  <tr class="metadata">
+    <th class="textfield" style="padding: 5px;width:10em;">Date</th>
+    <th class="textfield" style="padding: 5px;">Subject</th>
+    <th class="textfield" style="padding: 5px;width:6em;">&nbsp;</th>
+    <th class="textfield" style="padding: 5px;width:6em;">&nbsp;</th>
+  </tr>
 
-    <tr class="metadata">
-      <!--th class="textfield" style="padding: 5px;">&nbsp;</th-->
-      <th class="textfield" style="padding: 5px;width:10em;">Date</th>
-      <th class="textfield" style="padding: 5px;">Subject</th>
-      <th class="textfield" style="padding: 5px;width:6em;">&nbsp;</th>
-      <th class="textfield" style="padding: 5px;width:6em;">&nbsp;</th>
-    </tr>
-
-    <?php foreach ($campaigns as $campaign) { ?>
-    <tr>
-
-      <!--td style="text-align: center;"><img src="<?= ASSETURL.'/campaignimages/50/50/1/'.$campaign['filename'] ?>" alt="" /></td-->
-      <td style="text-align: center;">
-        <?php echo $campaign['campaign']; ?> </td>
-      <td class="textfield" style="padding: 5px;">
-        <?php echo Text::trim($campaign['subject'], 100); ?> </td>
-      <td style="padding: 5px; text-align: center;">
-        <a href="<?php echo SITEURL . "/newsletters/{$this->_newsletter}/{$campaign['id']}"; ?>">Edit</a> </td>
-      <td style="padding: 5px; text-align: center;">
-        <a href="<?php echo SITEURL . "/newsletters/{$this->_newsletter}/delete/{$campaign['id']}"; ?>">Delete</a> </td>
-    </tr>
-    <?php } ?>
-
-  </table>
-
-<?php } else { ?>
-
-  No campaigns
-
+<?php foreach ($campaigns as $campaign) { ?>
+  <tr>
+    <td style="text-align: center;">
+      <?php echo $campaign['campaign']; ?> </td>
+    <td class="textfield" style="padding: 5px;">
+      <?php echo Text::trim($campaign['subject'], 100); ?> </td>
+    <td style="padding: 5px; text-align: center;">
+      <a href="<?php echo SITEURL . "/newsletters/{$this->_newsletter}/{$campaign['id']}"; ?>">Edit</a> </td>
+    <td style="padding: 5px; text-align: center;">
+      <a href="<?php echo SITEURL . "/newsletters/{$this->_newsletter}/delete/{$campaign['id']}"; ?>">Delete</a> </td>
+  </tr>
 <?php } ?>
+
+<?php if (empty($campaigns)) { ?>
+  <tr>
+    <td colspan="4" style="text-align:center">No campaigns</td>
+  </tr>
+<?php } ?>
+
+</table>
