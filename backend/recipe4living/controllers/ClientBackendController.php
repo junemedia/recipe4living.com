36eaa5aab9d0c0993500c1b5418d77772750d329
John Shearer <john@ultranaut.com>
2017-09-18 10:17:58 -0400

    Add Copycat and Q&E to backend nav

--- backend/recipe4living/controllers/ClientBackendController.php
+++ backend/recipe4living/controllers/ClientBackendController.php
@@ -80,6 +80,8 @@ class ClientBackendController extends BackendController {
         SITEURL.'/newsletters/daily' => 'Daily',
         SITEURL.'/newsletters/singleserve' => 'Single Serving',
         SITEURL.'/newsletters/onepot' => 'One Pot Wonders',
+        SITEURL.'/newsletters/copycat' => 'Copycat Classics',
+        SITEURL.'/newsletters/quickeasy' => 'Quick & Easy'
       )
     );
 
@@ -91,104 +93,6 @@ class ClientBackendController extends BackendController {
         SITEURL.'/config/edit/adminEmail' => 'Contact form recipients'
       )
     );
-
-    /*
-    $nav[] = Array(
-      'name' => 'Categories',
-      'link' => SITEURL . '/categories',
-      'on' => Array('category_listing', 'category'),
-      'children' => Array(
-        SITEURL.'/articles/new_list' => 'category - live',
-      )
-    );
-
-    $nav[] = Array(
-      'name' => 'Groups',
-      'link' => SITEURL . '/meta',
-      'on' => Array('meta', 'meta_add_value', 'meta_edit_value'),
-      'children' => Array()
-    );
-
-    $nav[] = Array(
-      'name' => 'Blogs',
-      'link' => SITEURL.'/blogs',
-      'on' => Array('blog_listing', 'blog_details'),
-      'children' => Array(
-        FRONTENDSITEURL.'/blogs/share' => 'Create blog post',
-        SITEURL.'/blogs?view=deleted' => 'Deleted blog posts'
-      )
-    );
-
-    $nav[] = Array(
-      'name' => 'Tips',
-      'link' => SITEURL.'/quicktips',
-      'on' => Array('quicktip_listing', 'quicktip_details'),
-      'children' => Array(
-        SITEURL.'/quicktips/edit' => 'Create tip'
-      )
-    );
-
-    $nav[] = Array(
-      'name' => 'Reviews',
-      'link' => SITEURL . '/comments',
-      'on' => Array('comment_listing', 'comment_details'),
-      'children' => Array()
-    );
-
-    $nav[] = Array(
-      'name' => 'Search Terms',
-      'link' => SITEURL . '/searchterms',
-      'on' => Array('searchterm_listing'),
-      'children' => Array()
-    );
-
-    $nav[] = Array(
-      'name' => 'Text',
-      'link' => SITEURL . '/languages',
-      'on' => Array('languages'),
-      'children' => Array(
-      )
-    );
-
-    $nav[] = Array(
-      'name' => 'Images',
-      'link' => SITEURL . '/assets',
-      'on' => Array('images'),
-      'children' => Array()
-    );
-
-    $nav[] = Array(
-      'name' => 'Files',
-      'link' => SITEURL . '/assets/pdf',
-      'on' => Array('files'),
-      'children' => Array()
-    );
-
-    $nav[] = Array(
-      'name' => 'Article History',
-      'link' => SITEURL . '/articlehistory',
-      'on' => Array('article_history'),
-      'children' => Array()
-    );
-
-    $nav[] = Array(
-      'name' => 'Polls',
-      'link' => SITEURL . '/polls',
-      'on' => Array('polls'),
-      'children' => Array(
-        SITEURL.'/polls/poll' => 'Create poll'
-      )
-    );
-
-    $nav[] = Array(
-      'name' => 'Slideshows',
-      'link' => SITEURL . '/slideshows',
-      'on' => Array('slideshows'),
-      'children' => Array(
-        SITEURL.'/slideshows/slideshow' => 'Create slideshow'
-      )
-    );
-    */
 
     if(isset($this->_menuSlug)) {
       $menu_slug = $this->_menuSlug;
