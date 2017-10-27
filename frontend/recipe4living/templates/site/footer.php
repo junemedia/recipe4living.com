41d6338bc782d1eb268a51ad2dd88c2b4a22f14d
John Shearer <john@ultranaut.com>
2017-10-27 13:01:48 -0400

    Whitespace cleanup

--- frontend/recipe4living/templates/site/footer.php
+++ frontend/recipe4living/templates/site/footer.php
@@ -1,180 +1,180 @@
-		<div class="clear"></div>
-		<div id="footer-ad"><?= Template::get('footerAdvert'); ?></div>
-	</div>
+    <div class="clear"></div>
+    <div id="footer-ad"><?= Template::get('footerAdvert'); ?></div>
+  </div>
 </div>
 
 <div id="footer" class="site-wrapper screenonly">
 
-	<div style="text-align: center; margin: 0px; padding-bottom: 0px;background-color: background:#e2ded5;">
-		<img src="/frontend/recipe4living/images/site/FamilyOfSites.png" border="0" />
-	</div>
+  <div style="text-align: center; margin: 0px; padding-bottom: 0px;background-color: background:#e2ded5;">
+    <img src="/frontend/recipe4living/images/site/FamilyOfSites.png" border="0" />
+  </div>
 
-	<?php
-	// START OF RSS OF OTHER SITES
-	require_once(dirname(__FILE__).'/../../../../r4l/magpierss/rss_fetch.inc');
-	$ff_cacheFile = dirname(__FILE__) . '/../../../../cache_rss/ff_rss_xml.cache';
-	$ff_rss = fetch_rss("http://".$_SERVER['SERVER_NAME']."/cache_rss/ff_rss_xml.cache");
-	$wim_cacheFile = dirname(__FILE__) . '/../../../../cache_rss/wim_rss_xml.cache';
-	$wim_rss = fetch_rss("http://".$_SERVER['SERVER_NAME']."/cache_rss/wim_rss_xml.cache");
-	$rwm_cacheFile = dirname(__FILE__) . '/../../../../cache_rss/rwm_rss_xml.cache';
-	$rwm_rss = fetch_rss("http://".$_SERVER['SERVER_NAME']."/cache_rss/rwm_rss_xml.cache");
-	$cot_cacheFile = dirname(__FILE__) . '/../../../../cache_rss/cot_rss_xml.cache';
-	$cot_rss = fetch_rss("http://".$_SERVER['SERVER_NAME']."/cache_rss/cot_rss_xml.cache");
-	?>
+  <?php
+  // START OF RSS OF OTHER SITES
+  require_once(dirname(__FILE__).'/../../../../r4l/magpierss/rss_fetch.inc');
+  $ff_cacheFile = dirname(__FILE__) . '/../../../../cache_rss/ff_rss_xml.cache';
+  $ff_rss = fetch_rss("http://".$_SERVER['SERVER_NAME']."/cache_rss/ff_rss_xml.cache");
+  $wim_cacheFile = dirname(__FILE__) . '/../../../../cache_rss/wim_rss_xml.cache';
+  $wim_rss = fetch_rss("http://".$_SERVER['SERVER_NAME']."/cache_rss/wim_rss_xml.cache");
+  $rwm_cacheFile = dirname(__FILE__) . '/../../../../cache_rss/rwm_rss_xml.cache';
+  $rwm_rss = fetch_rss("http://".$_SERVER['SERVER_NAME']."/cache_rss/rwm_rss_xml.cache");
+  $cot_cacheFile = dirname(__FILE__) . '/../../../../cache_rss/cot_rss_xml.cache';
+  $cot_rss = fetch_rss("http://".$_SERVER['SERVER_NAME']."/cache_rss/cot_rss_xml.cache");
+  ?>
 
-	<div style="width: 980px;" id="mainFooterDiv">
-	<div class="rssdiv" id="ffrss">
+  <div style="width: 980px;" id="mainFooterDiv">
+    <div class="rssdiv" id="ffrss">
 
-		<?php
-			if (!defined('RSS_TITLE_LENGTH')) { define('RSS_TITLE_LENGTH', 30); }
-			$ff_rss = $ff_rss->items;
-			$i = 0;
-			echo '<ol class="rssfeed">';
-			foreach ($ff_rss as $item) { ?>
-				<div id="li_item">
-					<a target="_blank" title="<?php echo $item['title']; ?>" href="<?php echo $item['link']; ?>">
-						<?php $string = $item['title']; if (strlen($string) > RSS_TITLE_LENGTH) { $string = substr(wordwrap($string, RSS_TITLE_LENGTH), 0, strpos(wordwrap($string, RSS_TITLE_LENGTH), "\n")) . '...'; } else {$string = $item['title'];} echo $string; ?></a>
-				</div>
+      <?php
+        if (!defined('RSS_TITLE_LENGTH')) { define('RSS_TITLE_LENGTH', 30); }
+        $ff_rss = $ff_rss->items;
+        $i = 0;
+        echo '<ol class="rssfeed">';
+        foreach ($ff_rss as $item) { ?>
+          <div id="li_item">
+            <a target="_blank" title="<?php echo $item['title']; ?>" href="<?php echo $item['link']; ?>">
+              <?php $string = $item['title']; if (strlen($string) > RSS_TITLE_LENGTH) { $string = substr(wordwrap($string, RSS_TITLE_LENGTH), 0, strpos(wordwrap($string, RSS_TITLE_LENGTH), "\n")) . '...'; } else {$string = $item['title'];} echo $string; ?></a>
+          </div>
 
-				<?php
-				$i++;
-				if ($i >= 3) { break; }
-			}
-			echo '</li>';
-		?>
-	</div>
+          <?php
+          $i++;
+          if ($i >= 3) { break; }
+        }
+        echo '</li>';
+      ?>
+    </div>
 
-	<div class="rssdiv" id="wimrss">
-		<?php
-			$wim_rss = $wim_rss->items;
-			$i = 0;
-			echo '<ol class="rssfeed">';
-			foreach ($wim_rss as $item) { ?>
-				<div id="li_item">
-					<a target="_blank" title="<?php echo $item['title']; ?>" href="<?php echo $item['link']; ?>">
-						<?php $string = $item['title'];
-										if (strlen($string) > RSS_TITLE_LENGTH) {
-												$string = substr(wordwrap($string, RSS_TITLE_LENGTH), 0, strpos(wordwrap($string, RSS_TITLE_LENGTH), "\n")) . '...';
-										} else {$string = $item['title'];} echo $string; ?>
-								</a>
-						</div>
-				<?php
-				$i++;
-				if ($i>=3) break;
-			}
-			echo '</ol>';
-		?>
-	</div>
+    <div class="rssdiv" id="wimrss">
+      <?php
+        $wim_rss = $wim_rss->items;
+        $i = 0;
+        echo '<ol class="rssfeed">';
+        foreach ($wim_rss as $item) { ?>
+          <div id="li_item">
+            <a target="_blank" title="<?php echo $item['title']; ?>" href="<?php echo $item['link']; ?>">
+              <?php $string = $item['title'];
+                      if (strlen($string) > RSS_TITLE_LENGTH) {
+                          $string = substr(wordwrap($string, RSS_TITLE_LENGTH), 0, strpos(wordwrap($string, RSS_TITLE_LENGTH), "\n")) . '...';
+                      } else {$string = $item['title'];} echo $string; ?>
+                  </a>
+              </div>
+          <?php
+          $i++;
+          if ($i>=3) break;
+        }
+        echo '</ol>';
+      ?>
+    </div>
 
-	<div class="rssdiv" id="rwmrss">
-		<ol class="rssfeed">
-		<div id="li_item"><a target="_blank" title="Beautiful Appetizer Recipes" href="http://www.savvyfork.com/component/yoorecipe/category/11-appetizers.html">Beautiful Appetizer Recipes</a></div>
-		<div id="li_item"><a target="_blank" title="Sumptuous Main Dish Recipes" href="http://www.savvyfork.com/component/yoorecipe/category/13-main-dishes.html">Sumptuous Main Dish Recipes</a></div>
-		<div id="li_item"><a target="_blank" title="Gorgeous Dessert Recipes" href="http://www.savvyfork.com/component/yoorecipe/category/15-desserts.html">Gorgeous Dessert Recipes</a></div>
-		</ol>
-	</div>
+    <div class="rssdiv" id="rwmrss">
+      <ol class="rssfeed">
+      <div id="li_item"><a target="_blank" title="Beautiful Appetizer Recipes" href="http://www.savvyfork.com/component/yoorecipe/category/11-appetizers.html">Beautiful Appetizer Recipes</a></div>
+      <div id="li_item"><a target="_blank" title="Sumptuous Main Dish Recipes" href="http://www.savvyfork.com/component/yoorecipe/category/13-main-dishes.html">Sumptuous Main Dish Recipes</a></div>
+      <div id="li_item"><a target="_blank" title="Gorgeous Dessert Recipes" href="http://www.savvyfork.com/component/yoorecipe/category/15-desserts.html">Gorgeous Dessert Recipes</a></div>
+      </ol>
+    </div>
 
-	<div class="rssdiv" id="cotrss">
-	<?php
-		$cot = $cot_rss->items;
-		$i = 0;
-		echo '<ol class="rssfeed">';
-		foreach ($cot as $item) { ?>
-			<div id="li_item">
-							<a target="_blank" title="<?php echo $item['title']; ?>" href="<?php echo $item['link']; ?>">
-							<?php
-									$string = $item['title'];
-									if (strlen($string) > RSS_TITLE_LENGTH) {
-											$string = substr(wordwrap($string, RSS_TITLE_LENGTH), 0, strpos(wordwrap($string, RSS_TITLE_LENGTH), "\n")) . '...';
-									}else {
-											$string = $item['title'];
-									}
-									echo $string;
-							?>
-							</a>
-					</div>
-			<?php
-			$i++;
-			if ($i>=3) break;
-		}
-		echo '</ol>';
-	?>
-	</div>
-	<?php
-	//	END OF RSS OF OTHER SITES
-	?>
-	<div style="clear: left;"></div>
+    <div class="rssdiv" id="cotrss">
+    <?php
+      $cot = $cot_rss->items;
+      $i = 0;
+      echo '<ol class="rssfeed">';
+      foreach ($cot as $item) { ?>
+        <div id="li_item">
+                <a target="_blank" title="<?php echo $item['title']; ?>" href="<?php echo $item['link']; ?>">
+                <?php
+                    $string = $item['title'];
+                    if (strlen($string) > RSS_TITLE_LENGTH) {
+                        $string = substr(wordwrap($string, RSS_TITLE_LENGTH), 0, strpos(wordwrap($string, RSS_TITLE_LENGTH), "\n")) . '...';
+                    }else {
+                        $string = $item['title'];
+                    }
+                    echo $string;
+                ?>
+                </a>
+            </div>
+        <?php
+        $i++;
+        if ($i>=3) break;
+      }
+      echo '</ol>';
+    ?>
+    </div>
+  <?php
+  //  END OF RSS OF OTHER SITES
+  ?>
+    <div style="clear: left;"></div>
 
-	<div class="mainFooterDiv">
-		<a target="_blank" id="box-link-ff" href="http://www.fitandfabliving.com/"></a>
-		<a target="_blank" id="box-link-wim" href="http://www.workitmom.com/"></a>
-		<a target="_blank" id="box-link-rwm" href="http://www.savvyfork.com/"></a>
-		<a target="_blank" id="box-link-cot" href="http://www.chewonthatblog.com/"></a>
-	</div>
+    <div class="mainFooterDiv">
+      <a target="_blank" id="box-link-ff" href="http://www.fitandfabliving.com/"></a>
+      <a target="_blank" id="box-link-wim" href="http://www.workitmom.com/"></a>
+      <a target="_blank" id="box-link-rwm" href="http://www.savvyfork.com/"></a>
+      <a target="_blank" id="box-link-cot" href="http://www.chewonthatblog.com/"></a>
+    </div>
 
-	<div style="clear: left;"></div>
+    <div style="clear: left;"></div>
 
-	<div id="topLinks" style="text-align: center;padding-top:150px;">
-		<span style="padding-left: 0px;">
-			<a href="<?= SITEURL ?>/about">About Us</a>&nbsp;|&nbsp;
-			<a href="<?= SITEURL ?>/contact/">Contact Us</a>&nbsp;|&nbsp;
-			<a href="<?= SITEURL ?>/press">Press Room</a>&nbsp;|&nbsp;
-			<a href="<?= SITEURL ?>/sitemap">Site Map</a>&nbsp;|&nbsp;
-			<a href="<?= SITEURL ?>/index/links">Advertising</a>&nbsp;|&nbsp;
-			<a href="<?= SITEURL ?>/privacy">Privacy Policy</a>&nbsp;|&nbsp;
-			<a href="<?= SITEURL ?>/terms">Terms of Use</a>&nbsp;|&nbsp;
-			<a href="<?= SITEURL ?>/index/unsub">Unsubscribe</a>&nbsp;|&nbsp;
-			<a href="<?= SITEURL; ?>/index/subctr">Manage My Newsletters</a>
-		</span>
-		<p class="text-content"><br>&copy; <?php echo date('Y'); ?> <a href="http://www.junemedia.com/" target="_blank">June Media Inc</a> All rights reserved</p>
-	</div>
-	</div>
+    <div id="topLinks" style="text-align: center;padding-top:150px;">
+      <span style="padding-left: 0px;">
+        <a href="<?= SITEURL ?>/about">About Us</a>&nbsp;|&nbsp;
+        <a href="<?= SITEURL ?>/contact/">Contact Us</a>&nbsp;|&nbsp;
+        <a href="<?= SITEURL ?>/press">Press Room</a>&nbsp;|&nbsp;
+        <a href="<?= SITEURL ?>/sitemap">Site Map</a>&nbsp;|&nbsp;
+        <a href="<?= SITEURL ?>/index/links">Advertising</a>&nbsp;|&nbsp;
+        <a href="<?= SITEURL ?>/privacy">Privacy Policy</a>&nbsp;|&nbsp;
+        <a href="<?= SITEURL ?>/terms">Terms of Use</a>&nbsp;|&nbsp;
+        <a href="<?= SITEURL ?>/index/unsub">Unsubscribe</a>&nbsp;|&nbsp;
+        <a href="<?= SITEURL; ?>/index/subctr">Manage My Newsletters</a>
+      </span>
+      <p class="text-content"><br>&copy; <?php echo date('Y'); ?> <a href="http://www.junemedia.com/" target="_blank">June Media Inc</a> All rights reserved</p>
+    </div>
+  </div>
 
-	</div>
+</div>
 
-		<div class="clear"></div>
+<div class="clear"></div>
 
-		<?php if (ADS) { ?>
-			<script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
-			<script type="text/javascript">
-				_qacct="p-ed7ji9FtIlPSo";quantserve();
-			</script>
-		<?php } ?>
+<?php if (ADS) { ?>
+  <script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
+  <script type="text/javascript">
+    _qacct="p-ed7ji9FtIlPSo";quantserve();
+  </script>
+<?php } ?>
 
-			<!-- BEGIN SiteCTRL Script -->
-			<script type="text/javascript">
-			if(document.location.protocol=='http:'){
-			var Tynt=Tynt||[];Tynt.push('dRKpBGeA8r5kFwacwqm_6l');
-			(function(){var s=document.createElement('script');s.async="async";s.type="text/javascript";s.src='http://tcr.tynt.com/ti.js';var h=document.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})();
-			}
-			</script>
-			<!-- END SiteCTRL Script -->
-			<?php
+<!-- BEGIN SiteCTRL Script -->
+<script type="text/javascript">
+if(document.location.protocol=='http:'){
+var Tynt=Tynt||[];Tynt.push('dRKpBGeA8r5kFwacwqm_6l');
+(function(){var s=document.createElement('script');s.async="async";s.type="text/javascript";s.src='http://tcr.tynt.com/ti.js';var h=document.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})();
+}
+</script>
+<!-- END SiteCTRL Script -->
 
-					$encryption_key = "36851a80704f3cbe";
+<?php
+  $encryption_key = "36851a80704f3cbe";
 
-					function lr_encrypt($input, $encryption_key) {
-						$input = pkcs5_pad($input, mcrypt_get_block_size(MCRYPT_BLOWFISH, MCRYPT_MODE_CBC));
-						$td = mcrypt_module_open(MCRYPT_BLOWFISH, '', MCRYPT_MODE_CBC, '');
-						$iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
-						mcrypt_generic_init($td, $encryption_key, $iv);
-						$data = mcrypt_generic($td, $input);
-						mcrypt_generic_deinit($td);
-						mcrypt_module_close($td);
-						return array(bin2hex($data), bin2hex($iv), 'bf-cbc');
-					}
+  function lr_encrypt($input, $encryption_key) {
+    $input = pkcs5_pad($input, mcrypt_get_block_size(MCRYPT_BLOWFISH, MCRYPT_MODE_CBC));
+    $td = mcrypt_module_open(MCRYPT_BLOWFISH, '', MCRYPT_MODE_CBC, '');
+    $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+    mcrypt_generic_init($td, $encryption_key, $iv);
+    $data = mcrypt_generic($td, $input);
+    mcrypt_generic_deinit($td);
+    mcrypt_module_close($td);
+    return array(bin2hex($data), bin2hex($iv), 'bf-cbc');
+  }
 
-					function pkcs5_pad($text, $blocksize) {
-						$pad = $blocksize - (strlen($text) % $blocksize);
-						return $text . str_repeat(chr($pad), $pad);
-					}
-			?>
+  function pkcs5_pad($text, $blocksize) {
+    $pad = $blocksize - (strlen($text) % $blocksize);
+    return $text . str_repeat(chr($pad), $pad);
+  }
+?>
 
 
-	<div class="screenonly"><?= Template::get('underdogAdvert'); ?></div>
+<div class="screenonly"><?= Template::get('underdogAdvert'); ?></div>
 
-	<?php include BLUPATH_TEMPLATES.'/site/ads/liveconnect.php'; ?>
-	<?php include BLUPATH_TEMPLATES.'/site/ads/outbrain_js.php'; ?>
+<?php include BLUPATH_TEMPLATES.'/site/ads/liveconnect.php'; ?>
+<?php include BLUPATH_TEMPLATES.'/site/ads/outbrain_js.php'; ?>
 
 </body>
 </html>
--- frontend/recipe4living/templates/site/header.php
+++ frontend/recipe4living/templates/site/header.php
@@ -2,58 +2,58 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta http-equiv="x-ua-compatible" content="IE=edge" />
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
-	<?php if (strstr($_SERVER['REQUEST_URI'], '/links') ) { ?>
+  <?php if (strstr($_SERVER['REQUEST_URI'], '/links') ) { ?>
   <title>Advertise With Us</title>
   <?php } else { ?>
   <title><?= $title; ?></title>
   <?php } ?>
 
-	<?php if (strstr($_SERVER['REQUEST_URI'],'subctr')) { ?>
+  <?php if (strstr($_SERVER['REQUEST_URI'],'subctr')) { ?>
   <meta name="keywords" content="recipe newsletters, free recipe newsletter, recipe newsletter, daily recipe newsletter, crockpot recipe newsletter, slow cooker recipe newsletter, free slow cooker recipes, free crockpot recipes, free crockpot recipe newsletter budget cooking recipes, budget cooking newsletter,party recipes and tips, party recipe newsletter, quick and easy recipe newsletter, quick and easy recipes" />
   <?php } else { ?>
   <meta name="keywords" content="<?= $keywords ? htmlentities($keywords, ENT_QUOTES, 'utf-8') : 'Recipe, Recipes, Easy Recipes, Food Ideas, Quick Recipes, Healthy Recipes, Cooking Tips, Cooking Ideas, Food Tips,'; ?>" />
   <?php } ?>
 
-	<meta name="description" content="<?= $description ? htmlentities($description, ENT_QUOTES, 'utf-8') : 'Looking for recipes and cooking tips? Recipe4Living has a recipe for every occasion. Whether you are looking for a quick and easy recipe, healthy recipes, or food ideas & tips for a special occasion, we have you covered.' ?>" />
-	<meta name="author" content="Recipe4Living, Recipe4Living.com" />
-	<meta name="verify-v1" content="MdhXUubKMGRn6vL5WSVMEXeKt6D4mMrULy9MG+6+Zf8=" />
+  <meta name="description" content="<?= $description ? htmlentities($description, ENT_QUOTES, 'utf-8') : 'Looking for recipes and cooking tips? Recipe4Living has a recipe for every occasion. Whether you are looking for a quick and easy recipe, healthy recipes, or food ideas & tips for a special occasion, we have you covered.' ?>" />
+  <meta name="author" content="Recipe4Living, Recipe4Living.com" />
+  <meta name="verify-v1" content="MdhXUubKMGRn6vL5WSVMEXeKt6D4mMrULy9MG+6+Zf8=" />
 
   <?php if ($format !='print') {?>
-	<link rel="stylesheet" href="<?= SITEASSETURL; ?>/css/site.css,nav.css,index.css,articles.css,landing.css,recipes.css,account.css,static.css,stickywin.css?v=a80cb" type="text/css" media="screen" />
+  <link rel="stylesheet" href="<?= SITEASSETURL; ?>/css/site.css,nav.css,index.css,articles.css,landing.css,recipes.css,account.css,static.css,stickywin.css?v=a80cb" type="text/css" media="screen" />
   <? } ?>
 
-	<?php if ($format == 'print') {?>
+  <?php if ($format == 'print') {?>
   <link rel="stylesheet" href="<?= SITEASSETURL; ?>/css/print.css?v=4" type="text/css"<?php if ($format != 'print') { ?> media="print"<?php } ?> />
   <?php }?>
 
-	<link type="text/css" href="/frontend/recipe4living/css/print.css?v=4" media="print" rel="stylesheet">
-	<!--[if IE 6]><link href="<?= SITEASSETURL; ?>/css/ie6.css?v=4" rel="stylesheet" type="text/css" /><![endif]-->
-	<!--[if IE 7]><link href="<?= SITEASSETURL; ?>/css/ie7.css?v=4" rel="stylesheet" type="text/css" /><![endif]-->
-	<link rel="shortcut icon" href="<?= SITEASSETURL; ?>/images/favicon.ico" type="image/vnd.microsoft.icon" />
-	<link rel="icon" href="<?= SITEASSETURL; ?>/images/favicon.ico" type="image/vnd.microsoft.icon" />
+  <link type="text/css" href="/frontend/recipe4living/css/print.css?v=4" media="print" rel="stylesheet">
+  <!--[if IE 6]><link href="<?= SITEASSETURL; ?>/css/ie6.css?v=4" rel="stylesheet" type="text/css" /><![endif]-->
+  <!--[if IE 7]><link href="<?= SITEASSETURL; ?>/css/ie7.css?v=4" rel="stylesheet" type="text/css" /><![endif]-->
+  <link rel="shortcut icon" href="<?= SITEASSETURL; ?>/images/favicon.ico" type="image/vnd.microsoft.icon" />
+  <link rel="icon" href="<?= SITEASSETURL; ?>/images/favicon.ico" type="image/vnd.microsoft.icon" />
 
-	<?php if (Template::get('rssUrl')) { ?>
-	<link href="<?= SITEINSECUREURL.Template::get('rssUrl'); ?>" rel="alternate" type="application/rss+xml" title="<?= BluApplication::getSetting('storeName'); ?> | <?= Template::get('rssTitle', 'Latest Recipes'); ?>" />
-	<?php } ?>
+  <?php if (Template::get('rssUrl')) { ?>
+  <link href="<?= SITEINSECUREURL.Template::get('rssUrl'); ?>" rel="alternate" type="application/rss+xml" title="<?= BluApplication::getSetting('storeName'); ?> | <?= Template::get('rssTitle', 'Latest Recipes'); ?>" />
+  <?php } ?>
 
-	<?php if (($format != 'print') && (strpos($_SERVER['HTTP_USER_AGENT'], 'CrazyEgg Robot') !== 0)) { ?>
-	<script type="text/javascript">
-		/* Define global static variables. */
-		DEBUG = <?= DEBUG ? 'true' : 'false' ?>;
-		SITEURL = '<?= SITEURL ?>';
-		SITESECUREURL = '<?= SITESECUREURL ?>';
-		SITEINSECUREURL = '<?= SITEINSECUREURL ?>';
-		ASSETURL = '<?= ASSETURL ?>';
-		COREASSETURL = '<?= COREASSETURL ?>';
-		SITEASSETURL = '<?= SITEASSETURL ?>';
-	</script>
+  <?php if (($format != 'print') && (strpos($_SERVER['HTTP_USER_AGENT'], 'CrazyEgg Robot') !== 0)) { ?>
+  <script type="text/javascript">
+    /* Define global static variables. */
+    DEBUG = <?= DEBUG ? 'true' : 'false' ?>;
+    SITEURL = '<?= SITEURL ?>';
+    SITESECUREURL = '<?= SITESECUREURL ?>';
+    SITEINSECUREURL = '<?= SITEINSECUREURL ?>';
+    ASSETURL = '<?= ASSETURL ?>';
+    COREASSETURL = '<?= COREASSETURL ?>';
+    SITEASSETURL = '<?= SITEASSETURL ?>';
+  </script>
 
   <?php if ((substr($_SERVER['REQUEST_URI'],0,9) == '/recipes/' && strstr($_SERVER['REQUEST_URI'],'.htm'))) { ?>
-	<script type="text/javascript" src="<?= COREASSETURL ?>/js/StickyWin.js,Interface.js,Nav.js,HistoryManager.js,Forms.js,BrowseArea.js,Autocompleter.js,Milkbox.js,Wizard.js,sifr.js,Slideshow.js,Articles.js?ver=6wnp"></script>
+  <script type="text/javascript" src="<?= COREASSETURL ?>/js/StickyWin.js,Interface.js,Nav.js,HistoryManager.js,Forms.js,BrowseArea.js,Autocompleter.js,Milkbox.js,Wizard.js,sifr.js,Slideshow.js,Articles.js?ver=6wnp"></script>
   <?php } else { ?>
-	<script type="text/javascript" src="<?= COREASSETURL ?>/js/mootoolsCore.js,mootoolsMore.js,StickyWin.js,Interface.js,Nav.js,HistoryManager.js,Forms.js,BrowseArea.js,Autocompleter.js,Milkbox.js,Wizard.js,sifr.js,Slideshow.js,Articles.js?ver=6wnp"></script>
+  <script type="text/javascript" src="<?= COREASSETURL ?>/js/mootoolsCore.js,mootoolsMore.js,StickyWin.js,Interface.js,Nav.js,HistoryManager.js,Forms.js,BrowseArea.js,Autocompleter.js,Milkbox.js,Wizard.js,sifr.js,Slideshow.js,Articles.js?ver=6wnp"></script>
   <?php } ?>
 
   <script>
@@ -71,26 +71,26 @@
     }
   </script>
 
-	<script type="text/javascript" src="<?= COREASSETURL ?>/js/jquery.min.js,jquery.fancybox-1.3.4.pack.js,jquery.cookie.js"></script>
+  <script type="text/javascript" src="<?= COREASSETURL ?>/js/jquery.min.js,jquery.fancybox-1.3.4.pack.js,jquery.cookie.js"></script>
 
-	<script type="text/javascript">
-		var R4LSignUpDhtml = jQuery.noConflict();
-		var R4LDhtml = jQuery.noConflict();
-	</script>
+  <script type="text/javascript">
+    var R4LSignUpDhtml = jQuery.noConflict();
+    var R4LDhtml = jQuery.noConflict();
+  </script>
 
   <?php if (Template::get('tinyMce')) { ?>
-	<script type="text/javascript" src="<?= COREASSETURL ?>/plugins/tiny_mce/tiny_mce.js"></script>
-	<script type="text/javascript" src="<?= COREASSETURL; ?>/js/TinyMCE.js"></script>
-	<?php } ?>
+  <script type="text/javascript" src="<?= COREASSETURL ?>/plugins/tiny_mce/tiny_mce.js"></script>
+  <script type="text/javascript" src="<?= COREASSETURL; ?>/js/TinyMCE.js"></script>
+  <?php } ?>
 
-	<?php
-		// Page-specific script includes
-		echo $includeScript;
-		// Generic bits of header
-		echo $genericHeader;
-	?>
+  <?php
+    // Page-specific script includes
+    echo $includeScript;
+    // Generic bits of header
+    echo $genericHeader;
+  ?>
 
-	<script type="text/javascript">
+  <script type="text/javascript">
     window.addEvent('domready', function () {
 
       /* Init history manager */
@@ -133,7 +133,7 @@
     window.addEvent('load', function() {
       <?= $loadScript; ?>
     });
-	</script>
+  </script>
 
   <!-- Yieldbot.com Intent Tag LOADING -->
   <script type="text/javascript" src="https://cdn.yldbt.com/js/yieldbot.intent.js"></script>
@@ -148,13 +148,12 @@
   </script>
   <!-- END Yieldbot.com Intent Tag -->
 
-	<?php include BLUPATH_TEMPLATES.'/site/ads/medianet_js.php'; ?>
+  <?php include BLUPATH_TEMPLATES.'/site/ads/medianet_js.php'; ?>
 
-	<?php } ?>
+  <?php } ?>
 
   <meta name="msvalidate.01" content="E03168D9BB4076DC3C37E21B03C7EE91" />
   <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
-
 
   <?php
     $curr_title = Template::get('curr_title');
@@ -190,40 +189,39 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <!--Sticky Menu-->
 <script type="text/javascript" src="<?= COREASSETURL; ?>/js/stickymenu.js"></script>
 
-
 <?php include_once("dhtml.php"); ?>
 
 <div id="nav-header">
-	<div class="site-wrapper" id="top-ad"><?= Template::get('headerAdvert'); ?></div>
-	<div class="site-wrapper">
-		<div id="logo">
-			<a href="<?= SITEURL; ?>/"><img style="width:387px;margin-left: -5px;" class="screenonly" alt="<?= BluApplication::getSetting('storeName'); ?>" src="<?= SITEASSETURL; ?>/images/site/R4L-Homepage-Logo.png" /></a>
-			<a href="<?= SITEURL; ?>/"><img class="printonly" alt="<?= BluApplication::getSetting('storeName'); ?>" src="<?= SITEASSETURL; ?>/images/site/R4L-Homepage-Logo.png" /></a>
-		</div>
-		<?php $controls = (isset($_GET['controls'])) ? $_GET['controls'] : 1; ?>
-		<?php if ($format == 'print' && $controls == 1) { ?>
-		<div class="print-popup-controls">
-			<a href="#" onclick="window.print();" class="print">Print this page</a>
-			<a href="#" onclick="window.close();" class="close">Close this window</a>
-		</div>
-		<?php } ?>
-		<div class="tagline"><h4>Easy recipes and a helping of fun from home cooks like you.</h4></div>
-		<div class="standardform screenonly">
-			<div id="nav-links" class="screenonly text-content fr">
-				<ul>
-					<li>
-						<?php $currentUser = Template::get('currentUser'); ?>
-						<?php	if ($currentUser) { ?>
+  <div class="site-wrapper" id="top-ad"><?= Template::get('headerAdvert'); ?></div>
+  <div class="site-wrapper">
+    <div id="logo">
+      <a href="<?= SITEURL; ?>/"><img style="width:387px;margin-left: -5px;" class="screenonly" alt="<?= BluApplication::getSetting('storeName'); ?>" src="<?= SITEASSETURL; ?>/images/site/R4L-Homepage-Logo.png" /></a>
+      <a href="<?= SITEURL; ?>/"><img class="printonly" alt="<?= BluApplication::getSetting('storeName'); ?>" src="<?= SITEASSETURL; ?>/images/site/R4L-Homepage-Logo.png" /></a>
+    </div>
+    <?php $controls = (isset($_GET['controls'])) ? $_GET['controls'] : 1; ?>
+    <?php if ($format == 'print' && $controls == 1) { ?>
+    <div class="print-popup-controls">
+      <a href="#" onclick="window.print();" class="print">Print this page</a>
+      <a href="#" onclick="window.close();" class="close">Close this window</a>
+    </div>
+    <?php } ?>
+    <div class="tagline"><h4>Easy recipes and a helping of fun from home cooks like you.</h4></div>
+    <div class="standardform screenonly">
+      <div id="nav-links" class="screenonly text-content fr">
+        <ul>
+          <li>
+            <?php $currentUser = Template::get('currentUser'); ?>
+            <?php if ($currentUser) { ?>
             <a href="<?= SITEURL; ?>/account/">My Account</a> / <a href="<?= SITEURL ?>/facebook/logout/">Log out</a>
-						<?php } else { ?>
-							<a href="<?= SITEURL ?>/facebook/login/" id="sign_in"><span>Login</span></a> /
-							<a href="<?= SITEURL ?>/register/" id="join_now"><span>Register</span></a>
-						<?php } ?>
-					</li>
-				</ul>
-			</div>
-			<div class="formholder">
-				<form id="nav-top-form-search" action="<?= SITEURL; ?>/search" method="get" class="search fullsubmit">
+            <?php } else { ?>
+              <a href="<?= SITEURL ?>/facebook/login/" id="sign_in"><span>Login</span></a> /
+              <a href="<?= SITEURL ?>/register/" id="join_now"><span>Register</span></a>
+            <?php } ?>
+          </li>
+        </ul>
+      </div>
+      <div class="formholder">
+        <form id="nav-top-form-search" action="<?= SITEURL; ?>/search" method="get" class="search fullsubmit">
           <div>
             <div class="categories">
               <label class="radio"><input class="controllerradio" name="controller" type="radio" value="recipes"<?= Template::get('searchType', 'recipes') == 'recipes' ? ' checked="checked"' : ''; ?> /> Recipes</label>
@@ -236,14 +234,14 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             <button class="button-lg fl" type="submit" title="Find"><span>Search</span></button>
           </div>
         </form>
-			</div>
-		</div>
-		<div class="clear"></div>
-	</div>
+      </div>
+    </div>
+    <div class="clear"></div>
+  </div>
 </div>
 
 <?= $topNav; ?>
 
 
 <div id="content-wrapper">
-	<div class="site-wrapper">
+  <div class="site-wrapper">
