0f397c5571c2b7aab57907ff6f7b6d0162273739
John Shearer <john@ultranaut.com>
2017-11-09 12:12:49 -0500

    Fix styles on Add to Address Book page

--- frontend/recipe4living/templates/static/add_to_address_book.php
+++ frontend/recipe4living/templates/static/add_to_address_book.php
@@ -1,15 +1,12 @@
 <div id="main-content" class="static">
-  <div id="column-container">
     <div id="panel-center" class="column">
 
-      <div id="addtoaddressbook" class="rounded">
+      <div id="addtoaddressbook">
         <div class="content">
           <h1>Add Us To Your Address Book</h1>
           <div>
 
-            <h2>Add to Address Book</h2>
-
-            <p>To assure that your VerticalResponse email messages always make it to your Inbox, you just need to add <a href="mailto:news@e.hypster.com" style="font-weight:bold">news@e.hypster.com</a> to your email address book. Here's how...</p>
+            <p>To assure that your email messages always make it to your Inbox, you just need to add <a href="mailto:R4L@recipe4living-recipes.com" style="font-weight:bold"><em>R4L@recipe4living-recipes.com</em></a> to your email address book. Here's how...</p><br>
 
             <div align="left" style="padding:5px 0 0 0;">
               <a href="#gmail"><img src="http://www.verticalresponse.com/sites/www.verticalresponse.com/files/100x50-gmail.gif" width="80" height="40" hspace="5" border="0" /></a>
@@ -23,81 +20,81 @@
             <div style="margin:30px">
               <a name="gmail"></a>
 
-              <h3>Gmail<sup>&#153;</sup></h3>
-              <ul>
+              <h3 style="font-size: 1.2rem; margin-bottom:0.25em; border-bottom: solid 1px;">Gmail<sup>&#153;</sup></h3>
+              <ul style="margin-bottom: 1.5em;">
                 <li>Click the <b>Contacts</b> link located on the left navigation.</li>
                 <li>Click the first icon located under <b>Contacts</b> title.</li>
-                <li>Type news@e.hypster.com in the email box located on the far right.</li>
+                <li>Type <em>R4L@recipe4living-recipes.com</em> in the email box located on the far right.</li>
                 <li>Click the <b>Save</b> button.</li>
-                <li>news@e.hypster.com is now saved to your address book. </li>
+                <li><em>R4L@recipe4living-recipes.com</em> is now saved to your address book. </li>
               </ul>
 
               <a name="msn"></a>
 
-              <h3>MSN/Hotmail<sup>&#174;</sup></h3>
-              <ul>
+              <h3 style="font-size: 1.2rem; margin-bottom:0.25em; border-bottom: solid 1px;">MSN/Hotmail<sup>&#174;</sup></h3>
+              <ul style="margin-bottom: 1.5em;">
                 <li>After opening the message, click the <b>Options</b> button located in the upper right corner.</li>
                 <li>Under the 'junk email' section click the <b>Safe and Blocked Senders</b> link.</li>
                 <li>Click on the <b>Safe Senders</b> link.</li>
-                <li>news@e.hypster.com should automatically appear.</li>
-                <li>Enter news@e.hypster.com if it does not appear automatically.</li>
+                <li><em>R4L@recipe4living-recipes.com</em> should automatically appear.</li>
+                <li>Enter <em>R4L@recipe4living-recipes.com</em> if it does not appear automatically.</li>
                 <li>Click the <b>Add to List</b> button.</li>
-                <li>news@e.hypster.com is now saved to your address book.</li>
+                <li><em>R4L@recipe4living-recipes.com</em> is now saved to your address book.</li>
               </ul>
 
               <a name="Yahoo! Mail"></a>
 
-              <h3>Yahoo! Mail<sup>&#174;</sup></h3>
-              <ul>
+              <h3 style="font-size: 1.2rem; margin-bottom:0.25em; border-bottom: solid 1px;">Yahoo! Mail<sup>&#174;</sup></h3>
+              <ul style="margin-bottom: 1.5em;">
                 <li>Open the email.</li>
                 <li>With your mouse, highlight the email address at the top of the email.</li>
                 <li>Right-click the highlighted email address and select <b>Copy</b> from the drop-down menu.</li>
                 <li>On the left side of your screen, next to <b>Contacts</b>, click <b>Add</b>.</li>
                 <li>In the Email field, right-click and select <b>Paste</b>.</li>
                 <li>Click <b>Save</b>.</li>
-                <li>news@e.hypster.com is now saved in your address book.</li>
+                <li><em>R4L@recipe4living-recipes.com</em> is now saved in your address book.</li>
               </ul>
 
               <a name="Yahoo! Mail Classic"></a>
 
-              <h3>Yahoo! Mail Classic<sup>&#174;</sup></h3>
-              <ul>
+              <h3 style="font-size: 1.2rem; margin-bottom:0.25em; border-bottom: solid 1px;">Yahoo! Mail Classic<sup>&#174;</sup></h3>
+              <ul style="margin-bottom: 1.5em;">
                 <li>After opening the email, click the <b>Add sender to Contacts</b> icon located to the right of the VerticalResponse <i>from</i> address.</li>
                 <li>Click <b>Add to Address Book</b>.</li>
-                <li>news@e.hypster.com is now saved in your address book.</li>
+                <li><em>R4L@recipe4living-recipes.com</em> is now saved in your address book.</li>
               </ul>
 
               <a name="outlook"></a>
 
-              <h3>Outlook<sup>&#174;</sup></h3>
-              <ul>
-                <li>After opening the message, right-click news@e.hypster.com.</li>
+              <h3 style="font-size: 1.2rem; margin-bottom:0.25em; border-bottom: solid 1px;">Outlook<sup>&#174;</sup></h3>
+              <ul style="margin-bottom: 1.5em;">
+                <li>After opening the message, right-click <em>R4L@recipe4living-recipes.com</em>.</li>
                 <li>From the drop-down menu, select <b>Add to Outlook contacts...</b></li>
                 <li>Click the <b>Save and Close</b> button. </li>
-                <li>news@e.hypster.com is now saved to your address book. </li>
+                <li><em>R4L@recipe4living-recipes.com</em> is now saved to your address book. </li>
               </ul>
 
               <a name="aol"></a>
 
 
-              <h3>AOL 9.0<sup>&#174;</sup></h3>
-              <ul>
-                <li>Mouse over the Sender or From Name, [news@e.hypster.com]</li>
+              <h3 style="font-size: 1.2rem; margin-bottom:0.25em; border-bottom: solid 1px;">AOL 9.0<sup>&#174;</sup></h3>
+              <ul style="margin-bottom: 1.5em;">
+                <li>Mouse over the Sender or From Name, [R4L@recipe4living-recipes.com]</li>
                 <li>In the pop-up window, click the <b>Add Contact</b> link located on the right.</li>
                 <li>Add any additional information that you would like in the spaces provided.</li>
                 <li>Click <b>Add Contact</b> located at the bottom of the window.</li>
-                <li>news@e.hypster.com is now saved to your address book </li>
+                <li><em>R4L@recipe4living-recipes.com</em> is now saved to your address book </li>
               </ul>
 
 
               <a name="comcast"></a>
 
-              <h3>Comcast<sup>&#174;</sup></h3>
-              <ul>
+              <h3 style="font-size: 1.2rem; margin-bottom:0.25em; border-bottom: solid 1px;">Comcast<sup>&#174;</sup></h3>
+              <ul style="margin-bottom: 1.5em;">
                 <li>Open the email message and then click on the link directly below the sender's name, titled <b>Add to Address Book</b>.</li>
-                <li>Add news@e.hypster.com. The Email Address field should be pre-formatted with news@e.hypster.com.</li>
+                <li>Add <em>R4L@recipe4living-recipes.com</em>. The Email Address field should be pre-formatted with <em>R4L@recipe4living-recipes.com</em>.</li>
                 <li>Click <b>add</b> when you are finished entering all contact information.</li>
-                <li>news@e.hypster.com is now saved to your address book.</li>
+                <li><em>R4L@recipe4living-recipes.com</em> is now saved to your address book.</li>
               </ul>
             </div>
           </div>
@@ -105,19 +102,5 @@
       </div>
 
     </div>
-
-    <div id="panel-left" class="column">
-      <?php $this->leftnav(); ?>
-    </div>
-
-    <div id="panel-right" class="column">
-      <div class="ad"><?php $this->_advert('openx_300x250atf'); ?></div>
-
-      <?php $this->_box('reference_guides', array('limit' => 10)); ?>
-
-      <div class="ad"><?php $this->_advert('WEBSITE_RIGHT_BANNER_1'); ?></div>
-    </div>
-
     <div class="clear"></div>
-  </div>
 </div>
