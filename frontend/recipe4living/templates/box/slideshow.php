b9f5a2004e2adee3df21cb2dfd23825a328f46f9
John Shearer <john@ultranaut.com>
2018-01-12 13:03:20 -0500

    Remove old Campbell's Soup promo logo and link

--- frontend/recipe4living/templates/box/slideshow.php
+++ frontend/recipe4living/templates/box/slideshow.php
@@ -8,13 +8,7 @@
           <div id="panel-<?= $i; ?>" class="panel">
             <a href="<?= SITEURL.$item['link']; ?>"><img alt="<?= isset($item['featuredImage']['filename']) ? $item['featured_alt'] : $item['title']; ?>" src="<?= ASSETURL; ?>/itemimages/650/300/3/<?= isset($item['featuredImage']['filename']) ? $item['featuredImage']['filename'] : $item['image']['filename']; ?>" /></a>
 
-            <h2><a href="<?= SITEURL.$item['link']; ?>"><?= $item['title']; ?></a>
-
-            <?php if ($item['title'] == 'Easy Chicken & Cheese Enchiladas' || $item['title'] == 'Beef Taco Skillet' || $item['title'] == 'French Onion Burgers') { ?>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <a href='http://www.campbellkitchen.com/Home.aspx' target="_blank"><img src="http://pics.recipe4living.com/campbells_logo.bmp" border="0"></a>
-            <?php } ?>
-            </h2>
+            <h2><a href="<?= SITEURL.$item['link']; ?>"><?= $item['title']; ?></a></h2>
             <p class="text-content"><?= Text::trim($item['teaser'], 150); ?></p>
             <a href="<?= SITEURL.$item['link']; ?>" class="text-content more fr">View full <?= ($item['type'] == 'article') ? 'article' : 'recipe'; ?></a>
 
