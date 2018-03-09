d66b33c6f8f5e926be88dac86e6940898bda441c
John Shearer <john@ultranaut.com>
2018-03-09 10:06:07 -0500

    Disable Duda javascript

--- frontend/recipe4living/templates/site/header.php
+++ frontend/recipe4living/templates/site/header.php
@@ -5,8 +5,10 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
   <!-- Duda mobile redirect -->
+  <!--
   <script src="http://static.mobilewebsiteserver.com/redirect.js" type="text/javascript"></script>
   <script type="text/javascript">Mobile_redirect("http://m.recipe4living.com");</script>
+  -->
 
   <?php if (strstr($_SERVER['REQUEST_URI'], '/links') ) { ?>
   <title>Advertise With Us</title>
