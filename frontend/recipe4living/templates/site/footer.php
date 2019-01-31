50d33a907eb5cf83c013d196fa3b9e3ed56fbfa0
John Shearer <john@ultranaut.com>
2019-01-31 16:21:55 -0500

    Actually just remove the Qunatcast tag entirely

--- frontend/recipe4living/templates/site/footer.php
+++ frontend/recipe4living/templates/site/footer.php
@@ -31,31 +31,6 @@
 
 <div class="clear"></div>
 
-<!-- Quantcast Tag -->
-<script type="text/javascript">
-  var _qevents = _qevents || [];
-
-  (function() {
-    var elem = document.createElement('script');
-    elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
-    elem.async = true;
-    elem.type = "text/javascript";
-    var scpt = document.getElementsByTagName('script')[0];
-    scpt.parentNode.insertBefore(elem, scpt);
-  })();
-
-  _qevents.push({
-    qacct:"p-ed7ji9FtIlPSo"
-  });
-</script>
-
-<noscript>
-  <div style="display:none;">
-    <img src="//pixel.quantserve.com/pixel/p-ed7ji9FtIlPSo.gif" border="0" height="1" width="1" alt="Quantcast"/>
-  </div>
-</noscript>
-<!-- End Quantcast tag -->
-
 <?php include BLUPATH_TEMPLATES.'/site/ads/liveconnect.php'; ?>
 <?php include BLUPATH_TEMPLATES.'/site/ads/outbrain_js.php'; ?>
 
