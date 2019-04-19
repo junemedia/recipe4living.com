aec6ddc1fe560931661ddd9837fb1250d2834cfb
John Shearer <john@ultranaut.com>
2019-04-19 15:31:42 -0400

    Replace Connatix tag again

--- frontend/recipe4living/templates/site/ads/connatix_infeed.php
+++ frontend/recipe4living/templates/site/ads/connatix_infeed.php
@@ -1,10 +1,10 @@
 <script>
-  !function(n){if(!window.cnx){window.cnx={},window.cnx.cmd=[];var t=n.createElement('iframe');t.display='none',t.onload=function(){var n=t.contentWindow.document;c=n.createElement('script'),c.src='//cd.staging.connatix.com/connatix.player.js',c.setAttribute('async','1'),c.setAttribute('type','text/javascript'),n.body.appendChild(c)},n.head.appendChild(t)}}(document);
+!function(n){if(!window.cnx){window.cnx={},window.cnx.cmd=[];var t=n.createElement('iframe');t.display='none',t.onload=function(){var n=t.contentWindow.document;c=n.createElement('script'),c.src='//cd.connatix.com/connatix.player.js',c.setAttribute('async','1'),c.setAttribute('type','text/javascript'),n.body.appendChild(c)},n.head.appendChild(t)}}(document);
 </script>
-<script id="55a52ad012cf46be8f3a374a39cb4b24">
+<script id="60f38b3e55544d2a8364ad2552ddf91f">
   cnx.cmd.push(function() {
     cnx({
       playerId: '8c879260-23d7-449e-85f5-1a92d77e6b13'
-    }).render('55a52ad012cf46be8f3a374a39cb4b24');
+    }).render('60f38b3e55544d2a8364ad2552ddf91f');
   });
 </script>
