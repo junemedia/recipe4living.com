34dda8e7d2b88564449a84edc0bffc645e31f278
John Shearer <john@ultranaut.com>
2019-05-16 10:58:02 -0400

    Swap out Media.net javascript in head

--- frontend/recipe4living/templates/site/ads/medianet_js.php
+++ frontend/recipe4living/templates/site/ads/medianet_js.php
@@ -1,16 +1,6 @@
-<!-- ~ Copyright (C) 2014-2015 Media.net Advertising FZ-LLC All Rights Reserved -->
 <script type="text/javascript">
-  window._mNHandle = window._mNHandle || {};
-  window._mNHandle.queue = window._mNHandle.queue || [];
-  medianet_versionId = "121199";
-  (function() {
-    var sct = document.createElement("script"),
-        sctHl = document.getElementsByTagName("script")[0],
-        isSSL = 'https:' == document.location.protocol;
-    sct.type = "text/javascript";
-    sct.src = (isSSL ? 'https:' : 'http:') + '//contextual.media.net/dmedianet.js?cid=8CU52X6SM' + (isSSL ? '&https=1' : '')+'';
-    sct.async = "async";
-    sctHl.parentNode.insertBefore(sct, sctHl);
-  })();
+    window._mNHandle = window._mNHandle || {};
+    window._mNHandle.queue = window._mNHandle.queue || [];
+    medianet_versionId = "3121199";
 </script>
-
+<script src="//contextual.media.net/dmedianet.js?cid=8CU52X6SM" async="async"></script>
